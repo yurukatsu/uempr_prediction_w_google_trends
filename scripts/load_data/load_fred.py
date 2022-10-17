b794fc502ec4630a78d6590341f59935ec2d4722
yurukatsu <yuruyuru.kaihatsu@gmail.com>
2022-10-17 22:00:56 +0900

    Create script for loding FRED data

---
+++ scripts/load_data/load_fred.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+import sys
+
+sys.path.append("../../src/fred")
+import fred
+
+FRED_FACTORS = [
+    'UNRATE',
+    'PAYEMS',
+    'LNU04032231',
+    'ICSA',
+    'CCSA',
+    'IURSA',
+    'GDPC1',
+    'SP500',
+    'DJIA',
+    'NASDAQCOM',
+    'NASDAQ100',
+]
+
+def main():
+    def _load_fred(factor:str, save_dir:Path):
+            path_key = '../../src/fred/fred_key.txt'
+            df = fred.get_data(path_key, factor)
+            df.to_csv(save_dir / f'{factor}.csv', index=False)
+            
+    save_dir = Path("../../data/raw/macro")
+    for factor in FRED_FACTORS:
+        _load_fred(factor, save_dir)
+    
+if __name__ == "__main__":
+    main()

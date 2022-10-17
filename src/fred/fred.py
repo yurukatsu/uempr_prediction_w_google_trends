4b8e15f0ba99d0b5c2dba2406471c194f21076f1
yurukatsu <yuruyuru.kaihatsu@gmail.com>
2022-10-17 21:55:09 +0900

    Create module to download data from FRED

---
+++ src/fred/fred.py
@@ -0,0 +1,12 @@
+import numpy as np
+from full_fred.fred import Fred
+
+def get_data(path_key:str, factor:str):
+    fred = Fred(path_key)
+    df = fred.get_series_df(factor)
+    df = df.loc[:, ["date", "value"]]
+    df["value"].replace(".", np.nan, inplace=True)
+    df.dropna(how="any", inplace=True)
+    df["value"] = df["value"].astype(float)
+    df = df.rename(columns={"value":factor})
+    return df
